efe5acab995aae02500793784ba4bd6520cff46c
vladislaw <vlad2012chesnokov@mail.ru>
2023-04-25 04:08:15 +0500

    added some selenium

--- scrapper.py
+++ scrapper.py
@@ -5,6 +5,13 @@ import json
 from bs4 import BeautifulSoup
 import logging
 import os
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 path = os.getcwd() + "/datasets"
 os.chdir(path)
@@ -25,13 +32,8 @@ def get_one_package(package: bs4.element.ResultSet, page_number: int) -> dict():
     articles = dict()
     for el in package:
         article = {}
-        target_url = el.findChild("a", {"class": "news-link"}).attrs.get("href")
-        res = session.get(target_url, headers=headers)
-        if res.status_code != 200:
-            logging.error(f"HTTP status code {res.status_code} at {time.time() - start_time}")
-            time.sleep(15)
-            res = session.get(target_url, headers=headers)
-        soup = BeautifulSoup(res.text, 'html.parser')
+        driver.get(el.findChild("a", {"class": "news-link"}).attrs.get("href"))
+        driver.find_elements(By.CLASS_NAME, "text-extra-large line-low mb-2")
         article["header"] = soup.find('h1', {"class": "text-extra-large line-low mb-2"}).text
         article["author"] = soup.find("p", {"class": "article-byline text-low"}).text
         article["text"] = [i.text for i in soup.find("div", {"class": "mt-4 article-main"}).findAll("p")]
@@ -43,7 +45,7 @@ for categorie in all_categories:
     req = session.get(f"https://phys.org/{categorie}/sort/date/all/", headers=headers)
     if req.status_code != 200:
         logging.info("Error status code ", req.status_code)
-        time.sleep(10)
+        time.sleep(30)
         req = session.get(f"https://phys.org/{categorie}/sort/date/all/", headers=headers)
     count_of_pages = int(BeautifulSoup(req.text, 'html.parser').find("div", "pagination-view mr-4").find("span").text)
     for page in range(count_of_pages):
